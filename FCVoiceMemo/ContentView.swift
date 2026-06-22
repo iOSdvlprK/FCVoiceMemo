7cc9918a477be5810056e559328facc75e8902fc
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-06-22 23:18:28 +0900

    Initial Commit

---
+++ FCVoiceMemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FCVoiceMemo
+//
+//  Created by joe on 6/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FCVoiceMemo/FCVoiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FCVoiceMemoApp.swift
+//  FCVoiceMemo
+//
+//  Created by joe on 6/22/26.
+//
+
+import SwiftUI
+
+@main
+struct FCVoiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
