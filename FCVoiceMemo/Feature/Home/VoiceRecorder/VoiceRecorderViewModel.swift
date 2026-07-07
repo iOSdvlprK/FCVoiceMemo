2a33463dd511109412e4338414e0318889ad045d
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-07-07 23:13:31 +0900

    implement voice recorder view and integrate into onboarding
    - add VoiceRecorderView with lists, progress bar, and recording controls
    - refactor VoiceRecorderViewModel to generalize error alert properties
    - update OnboardingView to display VoiceRecorderView

--- FCVoiceMemo/Feature/Home/VoiceRecorder/VoiceRecorderViewModel.swift
+++ FCVoiceMemo/Feature/Home/VoiceRecorder/VoiceRecorderViewModel.swift
@@ -10,8 +10,8 @@ import Combine
 
 class VoiceRecorderViewModel: NSObject, ObservableObject, AVAudioPlayerDelegate {
     @Published var isDisplayRemoveVoiceRecorderAlert: Bool
-    @Published var isDisplayErrorAlert: Bool
-    @Published var errorAlertMessage: String
+    @Published var isDisplayAlert: Bool
+    @Published var alertMessage: String
     
     /// 음성메모 녹음 관련 프로퍼티
     var audioRecorder: AVAudioRecorder?
@@ -32,8 +32,8 @@ class VoiceRecorderViewModel: NSObject, ObservableObject, AVAudioPlayerDelegate 
     
     init(isDisplayRemoveVoiceRecorderAlert: Bool = false, isDisplayErrorAlert: Bool = false, errorAlertMessage: String = "", isRecording: Bool = false, isPlaying: Bool = false, isPaused: Bool = false, playedTime: TimeInterval = 0, recordedFiles: [URL] = [], selectedRecordedFile: URL? = nil) {
         self.isDisplayRemoveVoiceRecorderAlert = isDisplayRemoveVoiceRecorderAlert
-        self.isDisplayErrorAlert = isDisplayErrorAlert
-        self.errorAlertMessage = errorAlertMessage
+        self.isDisplayAlert = isDisplayErrorAlert
+        self.alertMessage = errorAlertMessage
         self.isRecording = isRecording
         self.isPlaying = isPlaying
         self.isPaused = isPaused
@@ -78,11 +78,11 @@ extension VoiceRecorderViewModel {
     }
     
     private func setErrorAlertMessage(_ message: String) {
-        errorAlertMessage = message
+        alertMessage = message
     }
     
     private func setIsDisplayErrorAlert(_ isDisplay: Bool) {
-        isDisplayErrorAlert = isDisplay
+        isDisplayAlert = isDisplay
     }
     
     private func displayAlert(message: String) {
--- FCVoiceMemo/Feature/Onboarding/OnboardingView.swift
+++ FCVoiceMemo/Feature/Onboarding/OnboardingView.swift
@@ -16,8 +16,7 @@ struct OnboardingView: View {
     var body: some View {
         NavigationStack(path: $pathModel.paths) {
 //            OnboardingContentView(onboardingViewModel: onboardingViewModel)
-            MemoListView()
-                .environmentObject(memoListViewModel)
+            VoiceRecorderView()
                 .navigationDestination(for: PathType.self) { pathType in
                     switch pathType {
                     case .homeView:
