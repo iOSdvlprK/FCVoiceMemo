44c3a7c6acdd02b36c315e0f21d32d40cde14506
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-07-10 23:46:55 +0900

    implement timer model, view model, and format extensions
    - create 'Time' struct to represent duration and handle conversions
    - implement 'TimerViewModel' to manage timer state, start, pause, resume, and cancel logic
    - add 'Int' extensions for formatting duration string ('HH : MM : SS') and target alarm time ('HH:mm')

---
+++ FCVoiceMemo/Model/Timer/Time.swift
@@ -0,0 +1,25 @@
+//
+//  Time.swift
+//  FCVoiceMemo
+//
+//  Created by joe on 7/10/26.
+//
+
+import Foundation
+
+struct Time {
+    var hours: Int
+    var minutes: Int
+    var seconds: Int
+    
+    var convertedSeconds: Int {
+        return (hours * 3600) + (minutes * 60) + seconds
+    }
+    
+    static func fromSeconds(_ seconds: Int) -> Time {
+        let hours = seconds / 3600
+        let minutes = (seconds % 3600) / 60
+        let remainingSeconds = (seconds % 3600) % 60
+        return Time(hours: hours, minutes: minutes, seconds: remainingSeconds)
+    }
+}
