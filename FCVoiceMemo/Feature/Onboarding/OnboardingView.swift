2a31ae19bfb7da787584162b942af698e823e9f6
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-07-15 23:53:35 +0900

    implement Home main tab navigation and view model
    - create Tab enum to define the five navigation sections
    - implement HomeViewModel to manage selected tab state and feature item counts
    - replace HomeView placeholder with a TabView linking the core subviews
    - restore OnboardingContentView destination in OnboardingView

--- FCVoiceMemo/Feature/Onboarding/OnboardingView.swift
+++ FCVoiceMemo/Feature/Onboarding/OnboardingView.swift
@@ -15,8 +15,7 @@ struct OnboardingView: View {
     
     var body: some View {
         NavigationStack(path: $pathModel.paths) {
-//            OnboardingContentView(onboardingViewModel: onboardingViewModel)
-            SettingView()
+            OnboardingContentView(onboardingViewModel: onboardingViewModel)
                 .navigationDestination(for: PathType.self) { pathType in
                     switch pathType {
                     case .homeView:
---
+++ FCVoiceMemo/Model/Home/Tab.swift
@@ -0,0 +1,16 @@
+//
+//  Tab.swift
+//  FCVoiceMemo
+//
+//  Created by joe on 7/15/26.
+//
+
+import Foundation
+
+enum Tab {
+    case todoList
+    case memo
+    case voiceRecorder
+    case timer
+    case setting
+}
